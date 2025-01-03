6ed767e9bfe2adf1e8e96468c6b6c4a6f6e6283d
glaxyt <glaxyt@naver.com>
2025-01-03 12:32:03 +0900

    feature: Reservation 예약 생성, 삭제 DTO 구현

---
+++ backend/src/main/java/com/ice/studyroom/domain/reservation/dto/response/ReservationResponse.java
@@ -0,0 +1,44 @@
+package com.ice.studyroom.domain.reservation.dto.response;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import com.ice.studyroom.domain.reservation.domain.entity.Reservation;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ReservationResponse {
+	private String message;
+	private ReservationInfo reservation;
+
+	@Getter
+	@Builder
+	public static class ReservationInfo {
+		private LocalDate scheduleDate;
+		private String roomNumber;
+		private LocalTime startTime;
+		private LocalTime endTime;
+	}
+
+	public static ReservationResponse of(Reservation reservation) {
+		ReservationInfo reservationInfo = ReservationInfo.builder()
+			.scheduleDate(reservation.getScheduleDate())
+			.roomNumber(reservation.getRoomNumber())
+			.startTime(reservation.getStartTime())
+			.endTime(reservation.getEndTime())
+			.build();
+
+		return ReservationResponse.builder()
+			.message("예약이 성공적으로 완료되었습니다.")
+			.reservation(reservationInfo)
+			.build();
+	}
+}
