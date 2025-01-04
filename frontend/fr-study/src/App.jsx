b4ae09c9d5b4680444e9dd12da06c1173857ad30
jay-onee <101964792+jay-onee@users.noreply.github.com>
2025-01-04 13:49:02 +0900

    feat: 프론트엔드 초기 설정 (#5)

---
+++ frontend/fr-study/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BookingProvider } from './pages/Reservation/BookingContext';
+import MainPage from './pages/Mainpage/mainpage';
+import ReservationStatus from './pages/ReservationStatus/ReservationStatus';
+import TimeSelection from './pages/Reservation/TimeSelection';
+import RoomSelection from './pages/Reservation/RoomSelection';
+import InfoInput from './pages/Reservation/Infoinput';
+
+
+function App() {
+  return (
+    <BookingProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/reservation/room" element={<RoomSelection />} />
+        <Route path="/reservation/time" element={<TimeSelection />} />
+        <Route path="/reservation/info" element={<InfoInput />} />
+        <Route path="/ReservationStatus" element={<ReservationStatus/>} />
+      </Routes>
+    </Router>
+    </BookingProvider>
+  );
+}
+
+export default App;
