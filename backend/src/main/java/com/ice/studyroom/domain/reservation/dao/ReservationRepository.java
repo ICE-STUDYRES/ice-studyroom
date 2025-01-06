9c9839b368f35b13804c79d276985660d4d0c26f
glaxyt <glaxyt@naver.com>
2025-01-07 03:55:03 +0900

    feat: 금일 예약, 스케줄 조회 jpa 메서드 구현

--- backend/src/main/java/com/ice/studyroom/domain/reservation/dao/ReservationRepository.java
+++ backend/src/main/java/com/ice/studyroom/domain/reservation/dao/ReservationRepository.java
@@ -1,8 +1,12 @@
 package com.ice.studyroom.domain.reservation.dao;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ice.studyroom.domain.reservation.domain.entity.Reservation;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+	List<Reservation> findByScheduleDate(LocalDate date);
 }
--- backend/src/main/java/com/ice/studyroom/domain/reservation/dao/ScheduleRepository.java
+++ backend/src/main/java/com/ice/studyroom/domain/reservation/dao/ScheduleRepository.java
@@ -1,8 +1,12 @@
 package com.ice.studyroom.domain.reservation.dao;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ice.studyroom.domain.reservation.domain.entity.Schedule;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+	List<Schedule> findByScheduleDate(LocalDate date);
 }
