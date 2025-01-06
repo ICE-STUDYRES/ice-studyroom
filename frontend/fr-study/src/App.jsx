ce93ea5c17587a72c9f82bd9c032fd149d5ac25b
KimWonBeen <been1215@gmail.com>
2025-01-06 17:06:44 +0900

    feat: 예약 페이지 UI 업데이트

--- frontend/fr-study/src/App.jsx
+++ frontend/fr-study/src/App.jsx
@@ -1,26 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { BookingProvider } from './pages/Reservation/BookingContext';
 import MainPage from './pages/Mainpage/mainpage';
 import ReservationStatus from './pages/ReservationStatus/ReservationStatus';
-import TimeSelection from './pages/Reservation/TimeSelection';
-import RoomSelection from './pages/Reservation/RoomSelection';
-import InfoInput from './pages/Reservation/Infoinput';
+import StudyRoomBooking from './pages/Reservation/StudyRoomBooking';
+import './main.css';
 
 
 function App() {
   return (
-    <BookingProvider>
+
     <Router>
       <Routes>
         <Route path="/" element={<MainPage />} />
-        <Route path="/reservation/room" element={<RoomSelection />} />
-        <Route path="/reservation/time" element={<TimeSelection />} />
-        <Route path="/reservation/info" element={<InfoInput />} />
+        <Route path="/reservation/room" element={<StudyRoomBooking/>} />
         <Route path="/ReservationStatus" element={<ReservationStatus/>} />
       </Routes>
     </Router>
-    </BookingProvider>
   );
 }
 
