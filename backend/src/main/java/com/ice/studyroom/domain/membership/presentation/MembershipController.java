8ab57905b6a0cfff9b79d9d8f3a9313b77d1bbe1
glaxyt <glaxyt@naver.com>
2025-01-08 19:24:44 +0900

    feat: 회원 생성 API 구현

---
+++ backend/src/main/java/com/ice/studyroom/domain/membership/presentation/MembershipController.java
@@ -0,0 +1,31 @@
+package com.ice.studyroom.domain.membership.presentation;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ice.studyroom.domain.membership.application.MembershipService;
+import com.ice.studyroom.domain.membership.presentation.dto.request.MemberCreateRequest;
+import com.ice.studyroom.domain.membership.presentation.dto.response.MemberCreateResponse;
+import com.ice.studyroom.global.dto.response.ResponseDto;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class MembershipController {
+	private final MembershipService membershipService;
+
+	@PostMapping
+	public ResponseEntity<ResponseDto<MemberCreateResponse>> createUser(
+		@Valid @RequestBody MemberCreateRequest request) {
+		return ResponseEntity
+			.status(HttpStatus.OK)
+			.body(ResponseDto.of(membershipService.createUser(request)));
+	}
+}
