0b1caa461f6e57b65868aabebe462e50412096ac
glaxyt <glaxyt@naver.com>
2025-01-08 19:57:53 +0900

    feat: 로그인 API 구현

--- backend/src/main/java/com/ice/studyroom/domain/membership/presentation/MembershipController.java
+++ backend/src/main/java/com/ice/studyroom/domain/membership/presentation/MembershipController.java
@@ -2,14 +2,18 @@ package com.ice.studyroom.domain.membership.presentation;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ice.studyroom.domain.identity.infrastructure.security.JwtTokenProvider;
 import com.ice.studyroom.domain.membership.application.MembershipService;
 import com.ice.studyroom.domain.membership.presentation.dto.request.MemberCreateRequest;
+import com.ice.studyroom.domain.membership.presentation.dto.request.MemberLoginRequest;
 import com.ice.studyroom.domain.membership.presentation.dto.response.MemberCreateResponse;
+import com.ice.studyroom.domain.membership.presentation.dto.response.MemberLoginResponse;
 import com.ice.studyroom.global.dto.response.ResponseDto;
 
 import jakarta.validation.Valid;
@@ -20,12 +24,21 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class MembershipController {
 	private final MembershipService membershipService;
+	private final JwtTokenProvider jwtTokenProvider;
+	private final AuthenticationManager authenticationManager;
 
 	@PostMapping
 	public ResponseEntity<ResponseDto<MemberCreateResponse>> createUser(
 		@Valid @RequestBody MemberCreateRequest request) {
 		return ResponseEntity
 			.status(HttpStatus.OK)
-			.body(ResponseDto.of(membershipService.createUser(request)));
+			.body(ResponseDto.of(membershipService.createMember(request)));
+	}
+
+	@PostMapping("/login")
+	public ResponseEntity<ResponseDto<MemberLoginResponse>> login(@Valid @RequestBody MemberLoginRequest request) {
+		return ResponseEntity
+			.status(HttpStatus.OK)
+			.body(ResponseDto.of(membershipService.login(request)));
 	}
 }
