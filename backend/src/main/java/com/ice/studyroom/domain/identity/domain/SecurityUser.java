dd465558c86818bd023fded2de113a00879b966a
glaxyt <glaxyt@naver.com>
2025-01-08 19:55:29 +0900

    feat: UserDetails 구현을 위한 SecurityUser 어댑터 추가

---
+++ backend/src/main/java/com/ice/studyroom/domain/identity/domain/SecurityUser.java
@@ -0,0 +1,55 @@
+package com.ice.studyroom.domain.identity.domain;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.ice.studyroom.domain.membership.domain.entity.Member;
+
+public class SecurityUser implements UserDetails {
+	private final Member user;
+
+	public SecurityUser(Member user) {
+		this.user = user;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return user.getRoles().stream()
+			.map(SimpleGrantedAuthority::new)
+			.collect(Collectors.toList());
+	}
+
+	@Override
+	public String getPassword() {
+		return user.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		return user.getEmail().getValue();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+}
