6ef213202013b8945614669573a2d45b5b58304c
glaxyt <glaxyt@naver.com>
2025-01-08 18:45:46 +0900

    feat: 회원 도메인 관리를 Member 도메인 구현

---
+++ backend/src/main/java/com/ice/studyroom/domain/membership/infrastructure/persistence/MemberRepository.java
@@ -0,0 +1,14 @@
+package com.ice.studyroom.domain.membership.infrastructure.persistence;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ice.studyroom.domain.membership.domain.entity.Member;
+import com.ice.studyroom.domain.membership.domain.vo.Email;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+	boolean existsByEmail(Email email);
+
+	Optional<Member> findByEmail(Email email);
+}
