a1bb83ced1bbcd8895b36b555c3d77983a9837fa
glaxyt <glaxyt@naver.com>
2025-01-15 12:19:54 +0900

    refactor: 로그인 응답 grantType삭제

--- backend/src/main/java/com/ice/studyroom/domain/membership/presentation/dto/response/MemberLoginResponse.java
+++ backend/src/main/java/com/ice/studyroom/domain/membership/presentation/dto/response/MemberLoginResponse.java
@@ -3,14 +3,12 @@ package com.ice.studyroom.domain.membership.presentation.dto.response;
 import com.ice.studyroom.domain.identity.domain.JwtToken;
 
 public record MemberLoginResponse(
-	String grantType,
 	String accessToken,
 	String refreshToken
 ) {
 	// factory 메서드를 통한 생성
 	public static MemberLoginResponse of(JwtToken jwtToken) {
 		return new MemberLoginResponse(
-			jwtToken.getGrantType(),
 			jwtToken.getAccessToken(),
 			jwtToken.getRefreshToken()
 		);
