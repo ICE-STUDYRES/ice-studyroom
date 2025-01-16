dd33f05af1aa6a98eb9868985213555a7a1ba67e
JungSoonIn <64190835+JungSoonIn@users.noreply.github.com>
2025-01-16 17:11:42 +0900

    feat: 이메일 전송 서비스 클래스 작성

--- backend/src/main/java/com/ice/studyroom/global/service/EmailService.java
+++ backend/src/main/java/com/ice/studyroom/global/service/EmailService.java
@@ -1,4 +1,38 @@
 package com.ice.studyroom.global.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+
+@Service
 public class EmailService {
+
+	private static final Logger logger = LoggerFactory.getLogger(EmailService.class);
+	private final JavaMailSender mailSender;
+
+	public EmailService(JavaMailSender mailSender) {
+		this.mailSender = mailSender;
+	}
+
+	public void sendEmail(String to, String subject, String body) {
+		try {
+			MimeMessage mimeMessage = mailSender.createMimeMessage();
+			MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+
+			helper.setTo(to);
+			helper.setSubject(subject);
+			helper.setText(body, true); // HTML 본문
+
+			mailSender.send(mimeMessage);
+			logger.info("알림 이메일 전송 성공: {}", to);
+		} catch (MessagingException e) {
+			logger.error("알림 이메일 전송 실패: {}", to, e);
+			throw new RuntimeException("메일 전송에 실패했습니다.", e);
+		}
+	}
 }
