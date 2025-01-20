2555c7aa65acd78af443c83feec6fe5fb3e271d3
이호진 <ihojin@ihojin-ui-MacBookPro.local>
2025-01-20 16:13:30 +0900

    feat : 관리자 기능 추가

---
+++ backend/src/main/java/com/ice/studyroom/domain/admin/presentation/dto/request/AdminCreateReserveRequest.java
@@ -0,0 +1,24 @@
+package com.ice.studyroom.domain.admin.presentation.dto.request;
+
+import com.ice.studyroom.domain.room_timeslot.domain.type.DayOfWeekStatus;
+
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalTime;
+
+public record AdminCreateReserveRequest(
+//	@NotNull(message = "방번호는 필수입니다")
+//	String roomNumber,
+//	@NotNull(message = "시작시간은 필수입니다.")
+//	LocalTime startTime,
+//	@NotNull(message = "종료시간은 필수입니다.")
+//	LocalTime endTime,
+//	@NotNull(message = "요일은 필수입니다.")
+//	DayOfWeekStatus dayOfWeek
+//	) {
+
+	@NotNull(message = "예약하고 싶은 날짜, 시간, 방번호의 번호를 선택해주세요.")
+	Long roomTimeSlotId
+) {
+}
+
