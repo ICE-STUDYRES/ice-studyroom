3b02b9504902d6c018c68287d3ef09f833582658
SungHyun Do <52828205+glaxyt@users.noreply.github.com>
2025-01-26 11:18:26 +0900

    [BE-#58] QR 생성 및 조회 구현 (#92)
    * fix: 더 이상 사용하지않는 room 도메인 삭제
    
    * feature: AES 암호화 설정
    
    * feature: QR Writer 설정
    
    * chore: QR 의존서 설치
    
    * chore: AES 암호키 설정
    
    * feature: 예약 시 QR Code 생성 및 조회 API 구현
    
    * fix: QR Code 유효기간 연장

--- backend/src/main/java/com/ice/studyroom/domain/room/dao/RoomRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ice.studyroom.domain.room.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ice.studyroom.domain.room.domain.entity.Room;
-
-public interface RoomRepository extends JpaRepository<Room, Long> {
-}
