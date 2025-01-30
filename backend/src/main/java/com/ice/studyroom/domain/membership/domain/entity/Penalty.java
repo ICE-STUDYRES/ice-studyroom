1fd34dd9488cc5c6759454e994cb60ce44032273
simjaesung <tlawotjd4285@gmail.com>
2025-01-30 20:27:54 +0900

    feat: penalty table index 추가

--- backend/src/main/java/com/ice/studyroom/domain/membership/domain/entity/Penalty.java
+++ backend/src/main/java/com/ice/studyroom/domain/membership/domain/entity/Penalty.java
@@ -14,6 +14,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -24,7 +25,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "penalty")
+@Table(
+	name = "penalty",
+	indexes = {
+		@Index(name = "idx_penalty_member_penalty_end", columnList = "member_id, penalty_end")
+	}
+)
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
