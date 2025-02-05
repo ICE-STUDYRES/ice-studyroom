8fe37e89745a4530767957b6522d2908e26f8950
ihojin <issac1118@kakao.com>
2025-02-05 21:04:12 +0900

    refactor: 퇴실시 스케줄러에 의한 정상퇴실확인 상태(COMPLETE) 추가

--- backend/src/main/java/com/ice/studyroom/domain/reservation/domain/type/ReservationStatus.java
+++ backend/src/main/java/com/ice/studyroom/domain/reservation/domain/type/ReservationStatus.java
@@ -3,9 +3,10 @@ package com.ice.studyroom.domain.reservation.domain.type;
 public enum ReservationStatus {
 
 	RESERVED,    // 예약됨
-	ENTRANCE,
-	LATE,
-	NO_SHOW,
-	EXIT,
+	ENTRANCE,    // 입장완료
+	LATE,        // 지각
+	NO_SHOW,     // 노쇼
+	EXIT,        // 퇴실완료
+	COMPLETE,    // 정상퇴실확인
 	CANCELLED    // 취소됨
 }
