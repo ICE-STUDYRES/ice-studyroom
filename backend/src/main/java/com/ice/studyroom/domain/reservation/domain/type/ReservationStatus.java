bcbc4cb2ef5f2f2284e399e3be84fe49d1478658
HoreungHoreung <128957586+HoreungHoreung@users.noreply.github.com>
2025-02-06 14:47:29 +0900

    [BE-#136] 개인/단체 예약 패널티 여부 및 최근예약상태 조회 (#145)
    * refactor: 패널티여부 및 최근예약상태 확인
    
    * refactor: 단체예약 api reservation -> reservation/group 으로 변경
    
    * refactor: 정상퇴실 확인상태(COMPLETED) 추가

--- backend/src/main/java/com/ice/studyroom/domain/reservation/domain/type/ReservationStatus.java
+++ backend/src/main/java/com/ice/studyroom/domain/reservation/domain/type/ReservationStatus.java
@@ -3,9 +3,10 @@ package com.ice.studyroom.domain.reservation.domain.type;
 public enum ReservationStatus {
 
 	RESERVED,    // 예약됨
-	ENTRANCE,
-	LATE,
-	NO_SHOW,
-	EXIT,
+	ENTRANCE,    // 입장완료
+	LATE,        // 지각
+	NO_SHOW,     // 노쇼
+	EXIT,        // 퇴장완료
+	COMPLETED,   // 정상퇴실 확인
 	CANCELLED    // 취소됨
 }
