39d378f3854da7e8edc0f35a3ac4e62f7eda9ba9
glaxyt <glaxyt@naver.com>
2025-02-11 02:38:35 +0900

    chore: 방 타입 변경 DTO Swagger 설명 추가

--- backend/src/main/java/com/ice/studyroom/domain/admin/presentation/dto/request/ChangeRoomTypeRequest.java
+++ backend/src/main/java/com/ice/studyroom/domain/admin/presentation/dto/request/ChangeRoomTypeRequest.java
@@ -1,6 +1,21 @@
 package com.ice.studyroom.domain.admin.presentation.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.ice.studyroom.domain.admin.domain.type.RoomType;
 
-public record ChangeRoomTypeRequest(long id, RoomType type) {
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+
+public record ChangeRoomTypeRequest(
+
+	@NotNull
+	@Schema(description = "방 ID", example = "2185", required = true)
+	@JsonProperty("id")
+	long id,
+
+	@NotNull
+	@Schema(description = "변경할 방 타입", example = "GROUP", required = true)
+	@JsonProperty("type")
+	RoomType type
+) {
 }
