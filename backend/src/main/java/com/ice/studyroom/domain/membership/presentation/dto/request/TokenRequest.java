8d8ef27ec2423c7840696588078422436b8c91d5
glaxyt <glaxyt@naver.com>
2025-02-13 01:57:31 +0900

    refactor: 사용하지 않는 DTO 삭제

--- backend/src/main/java/com/ice/studyroom/domain/membership/presentation/dto/request/TokenRequest.java
+++
@@ -1,9 +0,0 @@
-package com.ice.studyroom.domain.membership.presentation.dto.request;
-
-import jakarta.validation.constraints.NotNull;
-
-public record TokenRequest(
-	@NotNull(message = "Refresh Token은 필수입니다.")
-	String refreshToken
-) {
-}
