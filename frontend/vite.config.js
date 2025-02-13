86c481e424fc83a852096cae671000c9585102e6
김원빈 <benn@gim-wonbin-ui-MacBookPro.local>
2025-02-13 23:48:56 +0900

    feat: 자동배포 설정

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        secure: false,
-        // rewrite: (path) => path.replace(/^\/api/, '')  // 필요한 경우 주석 해제
+export default defineConfig(({ mode }) => {
+  // 환경 변수 로드
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_BASE_URL, // undefined 방지
+          changeOrigin: true,
+          secure: false,
+        }
       }
+    },
+    css: {
+      postcss: './postcss.config.cjs',
     }
-  },
-  css: {
-    postcss: './postcss.config.cjs',
-  }
+  };
 });
