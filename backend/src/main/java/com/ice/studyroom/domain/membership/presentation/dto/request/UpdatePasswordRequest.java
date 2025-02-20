bc4c2e069db7e22664dfe13722bcdfe295b9fa0f
simjaesung <tlawotjd4285@gmail.com>
2025-02-21 00:48:31 +0900

    fix: 비밀번호 규칙 일시적 무효화

--- backend/src/main/java/com/ice/studyroom/domain/membership/presentation/dto/request/UpdatePasswordRequest.java
+++ backend/src/main/java/com/ice/studyroom/domain/membership/presentation/dto/request/UpdatePasswordRequest.java
@@ -1,12 +1,14 @@
 package com.ice.studyroom.domain.membership.presentation.dto.request;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 public record UpdatePasswordRequest(
 	@NotNull(message = "기존 비밀번호는 필수입니다.")
 	String currentPassword,
 
 	@NotNull(message = "새로운 비밀번호는 필수입니다.")
+	//@Pattern(regexp = "^(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&])[a-z\\d@$!%*?&]{8,}$", message = "비밀번호는 최소 8자 이상, 하나 이상의 소문자, 숫자 및 특수 문자를 포함해야 합니다.")
 	String updatedPassword,
 
 	@NotNull(message = "확인용 새로운 비밀번호는 필수입니다.")
