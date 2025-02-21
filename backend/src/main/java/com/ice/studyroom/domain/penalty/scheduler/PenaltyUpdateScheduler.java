48646b3903471dfd40560afab79f8eb6ee3e1c5e
simjaesung <tlawotjd4285@gmail.com>
2025-02-21 23:27:55 +0900

    refactor: updateMemberPenalty 스케줄러 로직 개선

--- backend/src/main/java/com/ice/studyroom/domain/penalty/scheduler/PenaltyUpdateScheduler.java
+++ backend/src/main/java/com/ice/studyroom/domain/penalty/scheduler/PenaltyUpdateScheduler.java
@@ -10,17 +10,13 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import com.ice.studyroom.domain.membership.domain.entity.Member;
-import com.ice.studyroom.domain.membership.domain.service.MemberDomainService;
-import com.ice.studyroom.domain.membership.infrastructure.persistence.MemberRepository;
 import com.ice.studyroom.domain.penalty.application.PenaltyService;
-import com.ice.studyroom.domain.penalty.domain.type.PenaltyReasonType;
+import com.ice.studyroom.domain.penalty.domain.type.PenaltyStatus;
 import com.ice.studyroom.domain.penalty.infrastructure.persistence.PenaltyRepository;
 import com.ice.studyroom.domain.reservation.domain.entity.Reservation;
-import com.ice.studyroom.domain.reservation.domain.type.ReservationStatus;
 import com.ice.studyroom.domain.reservation.infrastructure.persistence.ReservationRepository;
 
 import jakarta.transaction.Transactional;
-import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -30,37 +26,33 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class PenaltyUpdateScheduler {
 
-	private final MemberRepository memberRepository;
 	private final PenaltyRepository penaltyRepository;
 	private final ReservationRepository reservationRepository;
 	private final PenaltyService penaltyService;
 
 	@Transactional
-	@Scheduled(cron = "0 0 0 * * *") // 매일 00:00에 실행
+	@Scheduled(cron = "0 27 23 * * *") // 매일 00:00에 실행
 	public void updateMemberPenalty() {
-		memberRepository.findAll().forEach(member -> {
-			boolean hasPenalty = penaltyRepository
-				.findTopByMemberIdAndPenaltyEndAfterOrderByPenaltyEndDesc(
-				member.getId(), LocalDateTime.now()).isPresent();
+		log.info("Processing update member penalty at {} ", LocalDateTime.now());
 
-			member.updatePenalty(hasPenalty);
+		penaltyRepository.findByStatus(PenaltyStatus.VALID).forEach(penalty -> {
+			if(penalty.isExpired()){
+				Member member = penalty.getMember();
+				member.updatePenalty(false);
+
+				log.info("해당 유저의 패널티가 해제되었습니다.: {} (ID: {}) at {}", member.getName(),
+					member.getStudentNum(), LocalDateTime.now());
+			}
 		});
 
 		log.info("Member Penalty updated successfully at {}", LocalDateTime.now());
 	}
 
-	@Transactional
-	@Scheduled(cron = "0 0 0 * * *") // 매일 00:00에 실행
-	public void expireOldPenalties() {
-		int updatedCount = penaltyRepository.expireOldPenalties(LocalDateTime.now());
-		log.info("{} 개의 penalty 가 만료되었습니다.", updatedCount);
-	}
-
 	@Scheduled(cron = "0 1 10-23 * * 1-5") // 평일 10:01 ~ 23:01
 	public void processNoShowPenalties() {
-		LocalDateTime now = LocalDateTime.now();
+		LocalDateTime now = LocalDateTime.now().withSecond(0).withNano(0);
 		LocalDate todayDate = now.toLocalDate();  //오늘 날짜
-		LocalTime todayTime = now.toLocalTime().withSecond(0).withNano(0); //현재 시간
+		LocalTime todayTime = now.toLocalTime(); //현재 시간
 
 		log.info("Processing no-show penalties for date: {} and time: {}", todayDate, todayTime);
 
