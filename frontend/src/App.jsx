10efd22ce200b43c512cd83d9201408544bccdd8
김원빈 <benn@gim-wonbin-ui-MacBookPro.local>
2025-02-21 19:43:21 +0900

    하드코딩과 목업만 작성해놓은 어드민 페이지 구현

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import StudyRoomBooking from './pages/Reservation/components/StudyRoomBookingUI'
 import MyReservationStatus from './pages/ReservationStatus/MyReservationStatus';
 import { Notification } from './pages/Notification/Notification';
 import AttendanceSystem from './pages/ReservationStatus/AttendanceSystem';
+import Adminpage from './pages/Mainpage/Adminpage/Adminpage';
 import './main.css';
 
 
@@ -17,6 +18,7 @@ function App() {
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/reservation/room" element={<StudyRoomBooking/>} />
+          <Route path="/adminpage" element={<Adminpage/>} />
           <Route path="/attendance" element={<AttendanceSystem/>} />
           <Route path="/reservation/manage" element={<StudyRoomManage/>} />
           <Route path="/ReservationStatus" element={<ReservationStatus/>} />
