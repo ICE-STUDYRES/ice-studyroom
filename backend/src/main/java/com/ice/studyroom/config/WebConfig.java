e42f28bfb5c241986ec635c002bf6c51a16623d4
simjaesung <tlawotjd4285@gmail.com>
2025-02-23 16:31:42 +0900

    feature: 패널티 인터셉터 제작

---
+++ backend/src/main/java/com/ice/studyroom/config/WebConfig.java
@@ -0,0 +1,33 @@
+package com.ice.studyroom.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.ice.studyroom.domain.identity.domain.service.TokenService;
+import com.ice.studyroom.domain.membership.domain.service.MemberDomainService;
+import com.ice.studyroom.domain.penalty.interceptor.PenaltyCheckInterceptor;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+	private final TokenService tokenService;
+	private final MemberDomainService memberDomainService;
+
+	@Bean
+	public PenaltyCheckInterceptor penaltyCheckInterceptor() {
+		return new PenaltyCheckInterceptor(tokenService, memberDomainService);
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(penaltyCheckInterceptor())
+			.order(1)
+			.addPathPatterns("/api/**")
+			.excludePathPatterns("/api/reservations/my/latest","api/reservations/my","/api/schedules", "/api/users/**");
+	}
+}
