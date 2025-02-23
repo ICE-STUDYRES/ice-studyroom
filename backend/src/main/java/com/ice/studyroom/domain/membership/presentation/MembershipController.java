46df84300a42d08d8f6393b877009f4b5e523add
glaxyt <glaxyt@naver.com>
2025-02-23 20:48:08 +0900

    hofix: cookie path 허용

--- backend/src/main/java/com/ice/studyroom/domain/membership/presentation/MembershipController.java
+++ backend/src/main/java/com/ice/studyroom/domain/membership/presentation/MembershipController.java
@@ -82,7 +82,7 @@ public class MembershipController {
 			.httpOnly(true)   // JavaScript에서 접근 불가 (XSS 방어)
 			.secure(true)     // 배포 시 true로 변경 예정, HTTPS 환경에서만 사용 가능
 			.sameSite("Strict") // CSRF 방어
-			.path("/api/users/refresh") // 특정 경로에서만 접근 가능
+			.path("/api/users/login") // 특정 경로에서만 접근 가능
 			.maxAge(Duration.ofDays(7)) // 7일간 유지
 			.build();
 		response.addHeader(HttpHeaders.SET_COOKIE, refreshTokenCookie.toString());
