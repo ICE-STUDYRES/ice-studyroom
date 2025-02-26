681d54e9f96bf47ab30919ca556caa21db1e7ed3
glaxyt <glaxyt@naver.com>
2025-02-27 00:19:28 +0900

    chore: 사용안되는 import 문 삭제

--- backend/src/main/java/com/ice/studyroom/domain/reservation/presentation/dto/response/GetReservationsResponse.java
+++ backend/src/main/java/com/ice/studyroom/domain/reservation/presentation/dto/response/GetReservationsResponse.java
@@ -1,7 +1,8 @@
 package com.ice.studyroom.domain.reservation.presentation.dto.response;
 
-import com.ice.studyroom.domain.reservation.domain.entity.Reservation;
 import java.util.List;
+
+import com.ice.studyroom.domain.reservation.domain.entity.Reservation;
 
 public record GetReservationsResponse(Reservation reservation, List<ParticipantResponse> participants) {
 	public static GetReservationsResponse from(Reservation reservation, List<ParticipantResponse> participants) {
