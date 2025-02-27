8e47029b3715ec363887580e2e4d5a837119c388
simjaesung <tlawotjd4285@gmail.com>
2025-02-28 00:23:49 +0900

    fix: cloudfront 에서 변환하는지 확인

--- backend/src/main/java/com/ice/studyroom/domain/penalty/interceptor/PenaltyCheckInterceptor.java
+++ backend/src/main/java/com/ice/studyroom/domain/penalty/interceptor/PenaltyCheckInterceptor.java
@@ -32,7 +32,7 @@ public class PenaltyCheckInterceptor implements HandlerInterceptor {
 		if(memberDomainService.isMemberPenalty(email)){
 			log.info("PenaltyCheckInterceptor 작동 - 이메일 : {} 이 제재를 받은 상태입니다.", email);
 			log.info("url : {}", request.getRequestURI());
-			throw new BusinessException(StatusCode.FORBIDDEN, "제재를 받은 사용자입니다.");
+			throw new BusinessException(StatusCode.UNAUTHORIZED, "제재를 받은 사용자입니다.");
 		}
 		return true;
 	}
