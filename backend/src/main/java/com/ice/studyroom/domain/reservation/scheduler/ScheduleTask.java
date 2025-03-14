8c39814795082c9e1a0a3869e77be440418cf9f5
glaxyt <glaxyt@naver.com>
2025-03-14 12:06:43 +0900

    fix: INSERT 쿼리문에 created_at, updated_at 명시

--- backend/src/main/java/com/ice/studyroom/domain/reservation/scheduler/ScheduleTask.java
+++ backend/src/main/java/com/ice/studyroom/domain/reservation/scheduler/ScheduleTask.java
@@ -20,31 +20,35 @@ public class ScheduleTask {
 	@Scheduled(cron = "${schedule.insert.cron}")
 	public void insertScheduleData() {
 		String sql = """
-			    INSERT INTO schedule (
-			        schedule_date,
-			        room_number,
-			        room_time_slot_id,
-			        room_type,
-			        start_time,
-			        end_time,
-			        capacity,
-			        min_res,
-			        status,
-			        day_of_week
-			    )
-			    SELECT\s
-			        CONVERT_TZ(CURRENT_DATE, 'UTC', 'Asia/Seoul'),
-			        r.room_number,
-			        r.id AS room_time_slot_id,
-			        r.room_type,
-			        r.start_time,
-			        r.end_time,
-			        r.capacity,
-			        r.min_res,
-			        r.status,
-			        DAYNAME(CONVERT_TZ(CURRENT_DATE, 'UTC', 'Asia/Seoul'))
-			    FROM room_time_slot r
-			    WHERE r.day_of_week = DAYNAME(CONVERT_TZ(CURRENT_DATE, 'UTC', 'Asia/Seoul'));
+				INSERT INTO schedule (
+					schedule_date,
+					room_number,
+					room_time_slot_id,
+					room_type,
+					start_time,
+					end_time,
+					capacity,
+					min_res,
+					status,
+					day_of_week,
+					created_at,
+					updated_at
+				)
+				SELECT\s
+					CONVERT_TZ(CURRENT_DATE, 'UTC', 'Asia/Seoul'),
+					r.room_number,
+					r.id AS room_time_slot_id,
+					r.room_type,
+					r.start_time,
+					r.end_time,
+					r.capacity,
+					r.min_res,
+					r.status,
+					DAYNAME(CONVERT_TZ(CURRENT_DATE, 'UTC', 'Asia/Seoul')),
+					NOW(),
+					NOW()
+				FROM room_time_slot r
+				WHERE r.day_of_week = DAYNAME(CONVERT_TZ(CURRENT_DATE, 'UTC', 'Asia/Seoul'));
 			""";
 		jdbcTemplate.update(sql);
 		System.out.println("Schedule inserted successfully at " + java.time.LocalDateTime.now());
