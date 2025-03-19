5261d22601f0712d11aa1b8bc277ef4b5b11f316
simjaesung <tlawotjd4285@gmail.com>
2025-03-19 21:36:00 +0900

    refactor: penalty jpa 함수 사용 개선

--- backend/src/main/java/com/ice/studyroom/domain/penalty/infrastructure/persistence/PenaltyRepository.java
+++ backend/src/main/java/com/ice/studyroom/domain/penalty/infrastructure/persistence/PenaltyRepository.java
@@ -4,19 +4,12 @@ import com.ice.studyroom.domain.penalty.domain.entity.Penalty;
 import com.ice.studyroom.domain.penalty.domain.type.PenaltyStatus;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
-import io.lettuce.core.dynamic.annotation.Param;
 
 public interface PenaltyRepository extends JpaRepository<Penalty, Long> {
-	Optional<Penalty> findTopByMemberIdAndPenaltyEndAfterOrderByPenaltyEndDesc(Long memberId, LocalDateTime currentTime);
-
-	List<Penalty> findByMemberIdAndPenaltyEndAfter(Long memberId, LocalDateTime currentTime);
 
 	List<Penalty> findByStatus(PenaltyStatus status);
 
