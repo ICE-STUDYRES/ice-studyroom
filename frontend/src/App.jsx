1ca6d517f12c712e3ce6f6f892981c67c8281ba2
Jayone Yang <jay1098@naver.com>
2025-03-20 01:07:52 +0900

    refactor: 전역변수 사용을 위해 생성한 context로 감싸기

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,26 +11,29 @@ import Adminpage from './pages/Mainpage/Adminpage/Adminpage';
 import Signin from './pages/Mainpage/components/Signin';
 import AdminSignin from './pages/Mainpage/components/AdminSignin';
 import Signup from './pages/Mainpage/components/Signup';
+import { UserProvider } from './pages/Mainpage/handlers/UserContext';
 import './main.css';
 
 function App() {
   return (
-    <Notification>
-      <Router>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/reservation/room" element={<StudyRoomBooking/>} />
-          <Route path="/adminpage" element={<Adminpage/>} />
-          <Route path="/attendance" element={<AttendanceSystem/>} />
-          <Route path="/reservation/manage" element={<StudyRoomManage/>} />
-          <Route path="/reservation/status" element={<ReservationStatus/>} />
-          <Route path="/reservation/my-status" element={<MyReservationStatus/>}/>
-          <Route path="/auth/signin" element={<Signin/>}/>
-          <Route path="/auth/signup" element={<Signup/>}/>
-          <Route path="/auth/admin-signin" element={<AdminSignin/>}/>
-        </Routes>
-      </Router> 
-    </Notification>
+    <UserProvider>
+      <Notification>
+        <Router>
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/reservation/room" element={<StudyRoomBooking />} />
+            <Route path="/adminpage" element={<Adminpage />} />
+            <Route path="/attendance" element={<AttendanceSystem />} />
+            <Route path="/reservation/manage" element={<StudyRoomManage />} />
+            <Route path="/reservation/status" element={<ReservationStatus />} />
+            <Route path="/reservation/my-status" element={<MyReservationStatus />} />
+            <Route path="/auth/signin" element={<Signin />} />
+            <Route path="/auth/signup" element={<Signup />} />
+            <Route path="/auth/admin-signin" element={<AdminSignin />} />
+          </Routes>
+        </Router> 
+      </Notification>
+    </UserProvider>
   );
 }
 
