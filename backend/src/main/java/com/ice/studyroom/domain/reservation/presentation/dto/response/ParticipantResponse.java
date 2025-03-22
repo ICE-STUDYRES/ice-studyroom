e7162854ef04186165f705b39b2070ef96c1572b
glaxyt <glaxyt@naver.com>
2025-03-22 22:15:05 +0900

    feature: 예약 조회 시 예약자 컬럼 전달 수정

--- backend/src/main/java/com/ice/studyroom/domain/reservation/presentation/dto/response/ParticipantResponse.java
+++ backend/src/main/java/com/ice/studyroom/domain/reservation/presentation/dto/response/ParticipantResponse.java
@@ -2,9 +2,9 @@ package com.ice.studyroom.domain.reservation.presentation.dto.response;
 
 import com.ice.studyroom.domain.membership.domain.entity.Member;
 
-public record ParticipantResponse(String studentNum, String name) {
-	public static ParticipantResponse from(Member member) {
-		return new ParticipantResponse(member.getStudentNum(), member.getName());
+public record ParticipantResponse(String studentNum, String name, boolean isHolder) {
+	public static ParticipantResponse from(Member member, boolean isHolder) {
+		return new ParticipantResponse(member.getStudentNum(), member.getName(), isHolder);
 	}
 }
 
