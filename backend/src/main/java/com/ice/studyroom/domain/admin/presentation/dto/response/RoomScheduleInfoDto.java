c3eb1dea487279fd9e584d23e9afda5676fcd229
simjaesung <89987940+simjaesung@users.noreply.github.com>
2025-03-23 01:34:26 +0900

    [BE-#405] 관리자 선점 기능 오류 해결 (#406)
    * fix: 관리자 선점 기능 오류 해결
    
    * fix: UI 용어 변경

--- backend/src/main/java/com/ice/studyroom/domain/admin/presentation/dto/response/RoomScheduleInfoDto.java
+++ backend/src/main/java/com/ice/studyroom/domain/admin/presentation/dto/response/RoomScheduleInfoDto.java
@@ -29,7 +29,7 @@ public record RoomScheduleInfoDto(
 
 	public static RoomScheduleInfoDto from(Schedule schedule){
 		return new RoomScheduleInfoDto(
-			schedule.getId(),
+			schedule.getRoomTimeSlotId(),
 			schedule.getRoomNumber(),
 			schedule.getStatus(),
 			schedule.getStartTime(),
