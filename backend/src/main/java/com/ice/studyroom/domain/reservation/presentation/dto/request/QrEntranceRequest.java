057ea19b9a456057587457d3fa633a673ca83394
glaxyt <glaxyt@naver.com>
2025-03-26 23:15:37 +0900

    refactor: 토큰 전달로 수정되었기에 qrToken으로 수정

--- backend/src/main/java/com/ice/studyroom/domain/reservation/presentation/dto/request/QrEntranceRequest.java
+++ backend/src/main/java/com/ice/studyroom/domain/reservation/presentation/dto/request/QrEntranceRequest.java
@@ -3,7 +3,7 @@ package com.ice.studyroom.domain.reservation.presentation.dto.request;
 import jakarta.validation.constraints.NotBlank;
 
 public record QrEntranceRequest(
-	@NotBlank(message = "QR Code는 필수입니다.")
-	String qrCode
+	@NotBlank(message = "QR Token은 필수입니다.")
+	String qrToken
 ) {
 }
