d72858c777d6e8f66d6407b867e52d99f595b90e
glaxyt <glaxyt@naver.com>
2025-04-09 14:44:35 +0900

    refactor: 무의미한 커스텀 예외 삭제

--- backend/src/main/java/com/ice/studyroom/domain/identity/exception/InvalidRefreshTokenException.java
+++
@@ -1,7 +0,0 @@
-package com.ice.studyroom.domain.identity.exception;
-
-public class InvalidRefreshTokenException extends RuntimeException {
-	public InvalidRefreshTokenException(String message) {
-		super(message);
-	}
-}
