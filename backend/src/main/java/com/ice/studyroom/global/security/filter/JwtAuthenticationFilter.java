85285068f75f4883da4ab1b4381f9096b4b064c2
glaxyt <glaxyt@naver.com>
2025-04-10 00:27:20 +0900

    refactor: 주석 정리 및 제거

--- backend/src/main/java/com/ice/studyroom/global/security/filter/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/ice/studyroom/global/security/filter/JwtAuthenticationFilter.java
@@ -39,13 +39,13 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
 		String requestURI = httpRequest.getRequestURI();
 		boolean isRefreshTokenRequest = "/api/users/auth/refresh".equals(requestURI);
 
-		// 1. Request Header에서 JWT 토큰 추출
+		// 1. Request Header 에서 JWT 토큰 추출
 		String token = resolveToken((HttpServletRequest)request);
 
-		// 2. validateToken으로 토큰 유효성 검사
+		// 2. validateToken 으로 토큰 유효성 검사
 		if (token != null) {
 			try {
-				jwtTokenProvider.validateToken(token, isRefreshTokenRequest); // 유효성 검사에서 예외 발생 시 catch로 이동
+				jwtTokenProvider.validateToken(token, isRefreshTokenRequest); // 유효성 검사에서 예외 발생 시 catch 로 이동
 
 				Authentication authentication = jwtTokenProvider.getAuthentication(token);
 				SecurityContextHolder.getContext().setAuthentication(authentication);
@@ -53,12 +53,12 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
 			} catch (ExpiredJwtException e) {
 				if (isRefreshTokenRequest) {
 					log.info("필터 내부 - 만료된 AT로 재발급 API 요청, URI: {}, Message: {}", httpRequest.getRequestURI(), e.getMessage());
-					// Refresh API는 만료된 AT를 허용하고 내부 로직에서 재발급
+					// Refresh API 는 만료된 AT를 허용하고 내부 로직에서 재발급
 					chain.doFilter(request, response);
 					return;
 				}
 
-				// 일반 요청인데 만료된 AT라면 401 Unauthorized 응답, 프론트는 위의 조건문으로 전달될 수 있는 요청할 예정
+				// 일반 요청인데 만료된 AT 라면 401 Unauthorized 응답, 프론트는 위의 조건문으로 전달될 수 있는 요청할 예정
 				log.warn("필터 내부 - 만료된 AT로 일반 API 요청, URI: {}, Message: {}", httpRequest.getRequestURI(), e.getMessage());
 				authenticationEntryPoint.commence(httpRequest, httpResponse,
 					new JwtAuthenticationException(StatusCode.UNAUTHORIZED, "Access token has expired"));
@@ -66,7 +66,7 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
 
 			} catch (JwtAuthenticationException e) {
 				log.warn("필터 내부 - JWT 인증 실패, URI: {}, Message: {}", httpRequest.getRequestURI(), e.getMessage());
-				// ExceptionTranslationFilter의 도움으로 예외 핸들러로 전달 가능
+				// ExceptionTranslationFilter 의 도움으로 예외 핸들러로 전달 가능
 				authenticationEntryPoint.commence(httpRequest, httpResponse, e);
 				return;
 			}
