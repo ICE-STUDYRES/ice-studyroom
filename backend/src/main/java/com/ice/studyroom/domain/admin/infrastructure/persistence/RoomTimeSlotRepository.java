1054dc5e0ca47e7a4bf3fec4aa4c672d9b8d98c3
glaxyt <glaxyt@naver.com>
2025-05-23 15:44:10 +0900

    feature: - 방 번호를 기준으로 DISTINCT 로 중복 없는 데이터 전달할 수 있게 구현 - 서비스 레이어에서 예외 처리할 부분이 없기에 곧바로 DTO로 매핑할 수 있게 구현

--- backend/src/main/java/com/ice/studyroom/domain/admin/infrastructure/persistence/RoomTimeSlotRepository.java
+++ backend/src/main/java/com/ice/studyroom/domain/admin/infrastructure/persistence/RoomTimeSlotRepository.java
@@ -2,15 +2,25 @@ package com.ice.studyroom.domain.admin.infrastructure.persistence;
 
 import com.ice.studyroom.domain.admin.domain.entity.RoomTimeSlot;
 import com.ice.studyroom.domain.admin.domain.type.DayOfWeekStatus;
+import com.ice.studyroom.domain.admin.domain.type.RoomType;
+import com.ice.studyroom.domain.admin.presentation.dto.response.RoomInfoResponse;
 import com.ice.studyroom.domain.reservation.domain.type.ScheduleSlotStatus;
 
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface RoomTimeSlotRepository extends JpaRepository<RoomTimeSlot, Long> {
 
 	List<RoomTimeSlot> findByStatus(ScheduleSlotStatus status);
-
 	List<RoomTimeSlot> findByDayOfWeek(DayOfWeekStatus dayOfWeekStatus);
-}
+
+	@Query("""
+    SELECT DISTINCT new com.ice.studyroom.domain.admin.presentation.dto.response.RoomInfoResponse(r.roomNumber, r.roomType, r.capacity)
+    FROM RoomTimeSlot r
+""")
+	List<RoomInfoResponse> findDistinctRoomInfo();
+ }
