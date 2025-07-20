eb024d966f9a5e1ede4548ba9dc0ad499e6ba309
glaxyt <glaxyt@naver.com>
2025-07-20 21:23:22 +0900

    refactor: ReservationExtendTest.java validateOwnership 적용된 내용 반영

--- backend/src/test/java/com/ice/studyroom/domain/reservation/application/ReservationExtendTest.java
+++ backend/src/test/java/com/ice/studyroom/domain/reservation/application/ReservationExtendTest.java
@@ -11,6 +11,7 @@ import java.time.ZoneId;
 import java.util.List;
 import java.util.Optional;
 
+import com.ice.studyroom.domain.reservation.domain.exception.reservation.ReservationAccessDeniedException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -160,7 +161,8 @@ public class ReservationExtendTest {
 
 		given(reservationRepository.findById(reservationId)).willReturn(Optional.of(reservation));
 		given(tokenService.extractEmailFromAccessToken(token)).willReturn(notOwnerEmail);
-		given(reservation.isOwnedBy(notOwnerEmail)).willReturn(false);
+		willThrow(new ReservationAccessDeniedException("해당 예약에 접근할 수 없습니다."))
+			.given(reservation).validateOwnership(notOwnerEmail);
 
 		// when & then
 		BusinessException ex = assertThrows(BusinessException.class, () ->
@@ -789,7 +791,7 @@ public class ReservationExtendTest {
 	private void 통과된_기본_예약_검증_셋업(Long reservationId, String token, String email) {
 		given(reservationRepository.findById(reservationId)).willReturn(Optional.of(reservation));
 		given(tokenService.extractEmailFromAccessToken(token)).willReturn(email);
-		given(reservation.isOwnedBy(email)).willReturn(true);
+		willDoNothing().given(reservation).validateOwnership(email);
 	}
 
 	private void 통과된_스케줄_연장_시간_검증_셋업() {
