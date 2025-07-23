6c4498efc349071e6eb95129fc80bc52aa45f560
glaxyt <glaxyt@naver.com>
2025-07-23 17:06:10 +0900

    refactor: 찾을 수 없는 예약의 경우 예외 및 메시지 구현

---
+++ backend/src/main/java/com/ice/studyroom/domain/reservation/domain/exception/reservation/ReservationNotFoundException.java
@@ -0,0 +1,38 @@
+package com.ice.studyroom.domain.reservation.domain.exception.reservation;
+
+import com.ice.studyroom.domain.reservation.domain.exception.type.ReservationAccessDeniedReason;
+import com.ice.studyroom.domain.reservation.domain.exception.type.ReservationActionType;
+import com.ice.studyroom.domain.reservation.domain.exception.type.ReservationNotFoundReason;
+import com.ice.studyroom.global.exception.BusinessException;
+import com.ice.studyroom.global.type.StatusCode;
+
+/**
+ * 요청한 Member 의 소유가 아닌 Reservation인 경우에는 접근할 수 없기에 예외 발생
+ */
+public class ReservationNotFoundException extends BusinessException {
+	/**
+	 * 어떤 기능을 서빙하다가 발생한 예외인지
+	 */
+	private final String description;
+	/**
+	 * 요청한 예약 ID
+	 */
+	private final Long reservationId;
+	/**
+	 * 예약에 대한 접근을 '시도한' 사용자의 이메일
+	 */
+	private final String requesterEmail;
+
+	public ReservationNotFoundException(ReservationNotFoundReason reason, Long reservationId, String requesterEmail, ReservationActionType actionType) {
+		super(StatusCode.NOT_FOUND, reason.getMessage());
+		this.description = actionType.getDescription();
+		this.reservationId = reservationId;
+		this.requesterEmail = requesterEmail;
+	}
+
+	public Long getReservationId() {
+		return reservationId;
+	}
+	public String getRequesterEmail() { return requesterEmail; }
+	public String getDescription() { return description; }
+}
