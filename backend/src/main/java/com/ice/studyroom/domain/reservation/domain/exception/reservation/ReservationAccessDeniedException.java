60cf851e34484a616040afc6281994e774e62114
glaxyt <glaxyt@naver.com>
2025-07-23 17:05:52 +0900

    refactor: 로깅을 위해 어떤 기능에서 실패했는지 데이터 필드 추가

--- backend/src/main/java/com/ice/studyroom/domain/reservation/domain/exception/reservation/ReservationAccessDeniedException.java
+++ backend/src/main/java/com/ice/studyroom/domain/reservation/domain/exception/reservation/ReservationAccessDeniedException.java
@@ -1,6 +1,8 @@
 package com.ice.studyroom.domain.reservation.domain.exception.reservation;
 
+import com.ice.studyroom.domain.reservation.domain.entity.Reservation;
 import com.ice.studyroom.domain.reservation.domain.exception.type.ReservationAccessDeniedReason;
+import com.ice.studyroom.domain.reservation.domain.exception.type.ReservationActionType;
 import com.ice.studyroom.global.exception.BusinessException;
 import com.ice.studyroom.global.type.StatusCode;
 
@@ -8,14 +10,29 @@ import com.ice.studyroom.global.type.StatusCode;
  * 요청한 Member 의 소유가 아닌 Reservation인 경우에는 접근할 수 없기에 예외 발생
  */
 public class ReservationAccessDeniedException extends BusinessException {
+	/**
+	 * 어떤 기능을 서빙하다가 발생한 예외인지
+	 */
+	private final String description;
+	/**
+	 * 접근이 거부된 예약의 ID
+	 */
 	private final Long reservationId;
+	/**
+	 * 예약에 대한 접근을 '시도한' 사용자의 이메일
+	 */
+	private final String requesterEmail;
 
-	public ReservationAccessDeniedException(ReservationAccessDeniedReason reason, Long reservationId) {
+	public ReservationAccessDeniedException(ReservationAccessDeniedReason reason, Long reservationId, String requesterEmail, ReservationActionType actionType) {
 		super(StatusCode.FORBIDDEN, reason.getMessage());
+		this.description = actionType.getDescription();
 		this.reservationId = reservationId;
+		this.requesterEmail = requesterEmail;
 	}
 
 	public Long getReservationId() {
 		return reservationId;
 	}
+	public String getRequesterEmail() { return requesterEmail; }
+	public String getDescription() { return description; }
 }
