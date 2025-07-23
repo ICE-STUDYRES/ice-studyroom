49f038f5d9fece2966f859f461b5cb571f5fbf75
glaxyt <glaxyt@naver.com>
2025-07-23 17:02:06 +0900

    refactor: 취소할 수 없는 예약 상태의 경우 발생하는 예외 및 메세지 구현

---
+++ backend/src/main/java/com/ice/studyroom/domain/reservation/domain/exception/reservation/cancel/InvalidCancelAttemptException.java
@@ -0,0 +1,25 @@
+package com.ice.studyroom.domain.reservation.domain.exception.reservation.cancel;
+
+import com.ice.studyroom.domain.reservation.domain.exception.type.cancel.InvalidCancelAttemptReason;
+import com.ice.studyroom.global.exception.BusinessException;
+import com.ice.studyroom.global.type.StatusCode;
+
+import java.time.LocalDateTime;
+
+
+/**
+ * Reservation.status 가 RESERVED 상태인 경우에만 취소가 가능합니다.
+ * 위의 경우 취소를 하기 전에 예약 상태를 검증 하는 과정에서 발생하는 예외를 정의했습니다.
+ */
+public class InvalidCancelAttemptException extends BusinessException {
+	private final Long reservationId;
+
+	public InvalidCancelAttemptException(InvalidCancelAttemptReason reason, Long reservationId) {
+		super(StatusCode.BAD_REQUEST, reason.getMessage());
+		this.reservationId = reservationId;
+	}
+
+	public Long getReservationId() {
+		return reservationId;
+	}
+}
---
+++ backend/src/main/java/com/ice/studyroom/domain/reservation/domain/exception/type/cancel/InvalidCancelAttemptReason.java
@@ -0,0 +1,19 @@
+package com.ice.studyroom.domain.reservation.domain.exception.type.cancel;
+
+public enum InvalidCancelAttemptReason {
+	ALREADY_USED("이미 사용되었거나 완료된 예약은 취소할 수 없습니다."),
+	ALREADY_CANCELLED("이미 취소된 예약입니다."),
+	TOO_LATE("입실 시간이 초과하였기에 취소할 수 없습니다."),
+	INVALID_STATE("취소 할 수 없는 예약 상태입니다.");
+
+	private final String message;
+
+	InvalidCancelAttemptReason(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+}
+
