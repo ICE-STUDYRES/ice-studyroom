f7d1802a6e9cd82e37fd5a6044151e06c51afaa1
glaxyt <glaxyt@naver.com>
2025-07-26 20:31:40 +0900

    feature: 카프카 데이터 전달을 위한 전송 객체 구현

---
+++ backend/src/main/java/com/ice/studyroom/domain/reservation/infrastructure/kafka/dto/VacancyNotificationRequest.java
@@ -0,0 +1,16 @@
+package com.ice.studyroom.domain.reservation.infrastructure.kafka.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class VacancyNotificationRequest {
+	private String email;
+	private String roomName;
+	private String eventDate;
+}
