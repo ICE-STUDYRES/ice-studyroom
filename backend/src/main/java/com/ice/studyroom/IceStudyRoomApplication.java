6ced6f8bbe4342b861e929d0ba3b0a67f279b841
glaxyt <glaxyt@naver.com>
2025-07-26 22:55:12 +0900

    chore: 비동기 실행할 수 있게 어노테이션 적용

--- backend/src/main/java/com/ice/studyroom/IceStudyRoomApplication.java
+++ backend/src/main/java/com/ice/studyroom/IceStudyRoomApplication.java
@@ -3,11 +3,12 @@ package com.ice.studyroom;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableJpaAuditing
+@EnableAsync
 @SpringBootApplication
 public class IceStudyRoomApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(IceStudyRoomApplication.class, args);
 	}
