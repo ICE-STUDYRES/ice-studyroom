3b2df65d7baba1ab7963e2eb934ae836a98e4b3b
glaxyt <glaxyt@naver.com>
2025-07-26 23:35:14 +0900

    fix: @EnableAsync 중복 설정 제거

--- backend/src/main/java/com/ice/studyroom/IceStudyRoomApplication.java
+++ backend/src/main/java/com/ice/studyroom/IceStudyRoomApplication.java
@@ -3,10 +3,8 @@ package com.ice.studyroom;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableJpaAuditing
-@EnableAsync
 @SpringBootApplication
 public class IceStudyRoomApplication {
 	public static void main(String[] args) {
