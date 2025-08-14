a7e195805cb9cbbed7dd87e71a06bb0e2dcdd7dc
glaxyt <glaxyt@naver.com>
2025-08-14 13:50:06 +0900

    refactor: 다중 스케줄 조회 시 존재하지않는 레코드가 존재할 경우를 처리하기 위한 필드 및 생성자 추가

--- backend/src/main/java/com/ice/studyroom/domain/schedule/domain/exception/schedule/ScheduleNotFoundException.java
+++ backend/src/main/java/com/ice/studyroom/domain/schedule/domain/exception/schedule/ScheduleNotFoundException.java
@@ -5,16 +5,29 @@ import com.ice.studyroom.global.type.ActionType;
 import com.ice.studyroom.global.type.StatusCode;
 import lombok.Getter;
 
+import java.util.List;
+
 @Getter
 public class ScheduleNotFoundException extends BusinessException {
 	private final Long scheduleId;
+	private final List<Long> scheduleIds;
 	private final String requesterEmail;
 	private final String description;
 
 	public ScheduleNotFoundException(Long scheduleId, String requesterEmail, ActionType actionType) {
 		super(StatusCode.NOT_FOUND, "유효하지않은 스케줄입니다.");
 		this.scheduleId = scheduleId;
+		this.scheduleIds = null;
 		this.requesterEmail = requesterEmail;
+		this.description = actionType.getDescription();
+	}
+
+	// 복수 스케줄용 생성자
+	public ScheduleNotFoundException(List<Long> scheduleIds, ActionType actionType) {
+		super(StatusCode.NOT_FOUND, "존재하지 않는 스케줄이 포함되어 있습니다.");
+		this.scheduleId = null;
+		this.scheduleIds = scheduleIds;
+		this.requesterEmail = null;
 		this.description = actionType.getDescription();
 	}
 }
