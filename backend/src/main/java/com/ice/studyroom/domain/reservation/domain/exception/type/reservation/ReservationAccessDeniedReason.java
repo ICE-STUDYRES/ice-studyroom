b6cad8d6a87449153d9c28c406ee1cdc0db72bfb
glaxyt <glaxyt@naver.com>
2025-08-14 13:45:52 +0900

    fix: 코드 가독성 향상을 위한 수정

--- backend/src/main/java/com/ice/studyroom/domain/reservation/domain/exception/type/reservation/ReservationAccessDeniedReason.java
+++ backend/src/main/java/com/ice/studyroom/domain/reservation/domain/exception/type/reservation/ReservationAccessDeniedReason.java
@@ -1,15 +1,12 @@
 package com.ice.studyroom.domain.reservation.domain.exception.type.reservation;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum ReservationAccessDeniedReason {
 	NOT_OWNER("유효하지 않은 사용자는 해당 예약에 접근할 수 없습니다.");
 
 	private final String message;
-
-	ReservationAccessDeniedReason(String message) {
-		this.message = message;
-	}
-
-	public String getMessage() {
-		return message;
-	}
 }
