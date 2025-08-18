f3e84f929267f2e107058b05e8b6d0fdba8a4e0c
glaxyt <glaxyt@naver.com>
2025-08-18 20:48:32 +0900

    refactor: 예외에 필요한 상수 및 커스텀 예외 구현

--- backend/src/main/java/com/ice/studyroom/domain/reservation/domain/exception/type/reservation/ReservationNotFoundReason.java
+++ backend/src/main/java/com/ice/studyroom/domain/reservation/domain/exception/type/reservation/ReservationNotFoundReason.java
@@ -1,7 +1,8 @@
 package com.ice.studyroom.domain.reservation.domain.exception.type.reservation;
 
 public enum ReservationNotFoundReason {
-	NOT_FOUND("존재하지 않는 예약입니다.");
+	NOT_FOUND("존재하지 않는 예약입니다."),
+	NOT_FOUND_BY_QR_TOKEN("특정 QR 토큰이 존재하지 않는 예약입니다.");
 
 	private final String message;
 
