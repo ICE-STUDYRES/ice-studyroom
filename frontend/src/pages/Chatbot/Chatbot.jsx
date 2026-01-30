01cb006ddb837b73d7af7ec6172303efa36c3e9f
JuyeonJang <erika.jjy3@gmail.com>
2026-01-31 01:25:12 +0900

    feat: add chatbot file structure

---
+++ frontend/src/pages/Chatbot/Chatbot.jsx
@@ -0,0 +1,13 @@
+// src/pages/Chatbot/Chatbot.jsx
+import React from "react";
+import "./Chatbot.css";
+
+const Chatbot = () => {
+  return (
+    <div className="chatbot-container">
+      챗봇 영역
+    </div>
+  );
+};
+
+export default Chatbot;
