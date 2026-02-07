b0501cd47879225291a8fa4efee7df3b82048e58
YoungVin Byeon <regan2003@naver.com>
2026-02-08 02:37:43 +0900

    fix: CI/CD build 오류 수정

--- backend/src/main/java/com/ice/studyroom/domain/chatbot/application/dto/GetCategoryResponse.java
+++ backend/src/main/java/com/ice/studyroom/domain/chatbot/application/dto/GetCategoryResponse.java
@@ -1,5 +1,26 @@
 package com.ice.studyroom.domain.chatbot.application.dto;
 
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class GetCategoryResponse {
 
+    private List<CategoryItem> categories;
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class CategoryItem {
+        private String categoryId;
+        private String label;
+    }
 }
