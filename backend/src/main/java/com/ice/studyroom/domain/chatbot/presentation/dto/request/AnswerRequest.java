f56722f1bc48f0d70af8652c638983c59908496a
KimJuHui <juhui4078@hufs.ac.kr>
2026-02-08 03:33:51 +0900

    feat: 챗봇 답변 DTO 추가

---
+++ backend/src/main/java/com/ice/studyroom/domain/chatbot/presentation/dto/request/AnswerRequest.java
@@ -0,0 +1,14 @@
+package com.ice.studyroom.domain.chatbot.presentation.dto.request;
+
+import com.ice.studyroom.domain.chatbot.domain.type.CategoryType;
+import jakarta.validation.constraints.NotNull;
+
+// 1. 불변 DTO (record 사용)
+public record AnswerRequest (
+	// 2. 필드
+	@NotNull
+	CategoryType categoryId,
+	// 2-1. NotNull 어노테이션 : null이면 해당 요청은 유효 X
+	@NotNull
+	Long questionId
+) {}
