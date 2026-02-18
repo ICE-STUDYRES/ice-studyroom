f9a50884d122fa93fd038d97da88c1def9132988
yejun05011 <dpwnsdl0501@naver.com>
2026-02-18 18:54:51 +0900

    refactor: ZoneId 명시하는 코드로 수정

--- backend/src/main/java/com/ice/studyroom/domain/ranking/scheduler/RankingScheduler.java
+++ backend/src/main/java/com/ice/studyroom/domain/ranking/scheduler/RankingScheduler.java
@@ -9,6 +9,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 
 import java.time.LocalDate;
 import java.time.YearMonth;
+import java.time.ZoneId;
 
 @Configuration
 @EnableScheduling
@@ -16,51 +17,51 @@ import java.time.YearMonth;
 public class RankingScheduler {
 
 	private final RankingSnapshotJob snapshotJob;
+	private static final String KST_ZONE = "Asia/Seoul";
+	private static final ZoneId KST = ZoneId.of(KST_ZONE);
 
 	// 주간 - 일요일 23:59:59
-	@Scheduled(cron = "59 59 23 ? * SUN")
+	@Scheduled(cron = "59 59 23 ? * SUN", zone = KST_ZONE)
 	public void weeklySnapshot() {
 		snapshotJob.execute(
 			RankingPeriod.WEEKLY,
-			LocalDate.now().toString()
+			LocalDate.now(KST).toString()
 		);
 	}
 
 	// 월간 - 말일 23:59:59
-	@Scheduled(cron = "59 59 23 L * ?")
+	@Scheduled(cron = "59 59 23 L * ?", zone = KST_ZONE)
 	public void monthlySnapshot() {
 		snapshotJob.execute(
 			RankingPeriod.MONTHLY,
-			YearMonth.now().toString()
+			YearMonth.now(KST).toString()
 		);
 	}
 
 	// 연간 - 12월 31일 23:59:59
-	@Scheduled(cron = "59 59 23 31 12 ?")
+	@Scheduled(cron = "59 59 23 31 12 ?", zone = KST_ZONE)
 	public void yearlySnapshot() {
 		snapshotJob.execute(
 			RankingPeriod.YEARLY,
-			String.valueOf(LocalDate.now().getYear())
+			String.valueOf(LocalDate.now(KST).getYear())
 		);
 	}
 
 	// 1학기 - 매년 7월 1일 00:00
-	@Scheduled(cron = "0 0 0 1 7 ?")
+	@Scheduled(cron = "0 0 0 1 7 ?", zone = KST_ZONE)
 	public void firstSemesterSnapshot() {
 		snapshotJob.execute(
 				RankingPeriod.SEMESTER,
-				LocalDate.now().getYear() + "-1"
+				LocalDate.now(KST).getYear() + "-1"
 		);
 	}
 
 	// 2학기 - 매년 11월 1일 00:00
-	@Scheduled(cron = "0 0 0 1 11 ?")
+	@Scheduled(cron = "0 0 0 1 11 ?", zone = KST_ZONE)
 	public void secondSemesterSnapshot() {
 		snapshotJob.execute(
 				RankingPeriod.SEMESTER,
-				LocalDate.now().getYear() + "-2"
+				LocalDate.now(KST).getYear() + "-2"
 		);
 	}
-
-
 }
