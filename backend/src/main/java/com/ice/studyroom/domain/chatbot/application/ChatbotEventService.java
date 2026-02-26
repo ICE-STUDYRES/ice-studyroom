8aed843e0f6e0b112629496722afeb7e82fa1cb6
KimJuHui <juhui4078@hufs.ac.kr>
2026-02-26 20:35:49 +0900

    refactor : Transactional 어노테이션 제거

--- backend/src/main/java/com/ice/studyroom/domain/chatbot/application/ChatbotEventService.java
+++ backend/src/main/java/com/ice/studyroom/domain/chatbot/application/ChatbotEventService.java
@@ -5,14 +5,12 @@ import com.ice.studyroom.domain.chatbot.domain.event.ChatbotEventLogRepository;
 import com.ice.studyroom.domain.chatbot.presentation.dto.request.ChatbotEventRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class ChatbotEventService {
 	private final ChatbotEventLogRepository eventLogRepository;
 
-	@Transactional
 	public void logEvent(ChatbotEventRequest request){
 		ChatbotEventLog eventLog = new ChatbotEventLog(
 			request.eventType(),
