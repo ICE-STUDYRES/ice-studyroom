5b95abecce4e84c7b9aa2bc7a07da6392ae25855
yejun05011 <dpwnsdl0501@naver.com>
2026-02-27 18:48:10 +0900

    feat: 랭킹 이벤트 ID 생성 규칙 표준화

--- backend/src/main/java/com/ice/studyroom/domain/ranking/application/event/publisher/EventIdGenerator.java
+++ backend/src/main/java/com/ice/studyroom/domain/ranking/application/event/publisher/EventIdGenerator.java
@@ -1,4 +1,4 @@
-package com.ice.studyroom.domain.ranking.application.event;
+package com.ice.studyroom.domain.ranking.application.event.publisher;
 
 import org.springframework.stereotype.Component;
 
